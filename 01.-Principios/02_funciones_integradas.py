0fb6d29b49b51a78283e8ed72667a099d0a54a35
Mario Martinez <112269506+mariomesm@users.noreply.github.com>
2024-07-16 12:36:01 -0600

    fixed correction with local and repository

--- 01.-Principios/02_funciones_integradas.py
+++ 01.-Principios/02_funciones_integradas.py
@@ -96,7 +96,6 @@ print(persona.__dict__) #Me muestra el diccionario de persona
 delattr(persona,'nat') #Elimina de mi diccionario la propiedad nat
 print(persona.__dict__)
 
-<<<<<<< HEAD
 class Persona:
     def __init__(self, nombre, edad):
         self.nombre = nombre
@@ -110,6 +109,4 @@ juan = Persona("Juan", 30)
 
 # Mostrar los nombres de atributos y métodos de la instancia juan
 print(dir(juan))
-=======
->>>>>>> 5aa36ef (function study)
 
